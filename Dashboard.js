28bdc39f160f697d7e8441c7b115837facba8b74
Ian Kropp <iankropp@gmail.com>
2023-08-04 10:00:42 -0400

    Have the table working vueingly

--- Dashboard.js
+++ Dashboard.js
@@ -2,9 +2,20 @@
 const { createApp, reactive, ref } = Vue;
 
 createApp({
-  // Created 
+  // Data 
+  data(){
+    return {
+      imageWidgets : new Set()
+        
+
+      , 
+      tableWidgets : { }
+
+
+    }
+  }, 
+  // Created hook 
   created(){
-    console.log("Hello Vue!")
 
     const evtSource = new EventSource("listen");
     
@@ -14,31 +25,30 @@ createApp({
     
       let title = data.title
     
-      // Append to the overview able first off
+      // Append to the overview table first off
       if(title == "Overview"){
+  
+        this.tableWidgets["Overview"] = data.content
+
+        //var tableContent = data.content
+        //
+        //var tabWrapper = document.getElementById("overview-tab-wrapper") 
     
-        var tableContent = data.content
-        
-        var tabWrapper = document.getElementById("overview-tab-wrapper") 
+        //if(document.getElementById("overview-tab")  !== null){
+        //  document.getElementById("overview-tab").remove()
+        //}else{
+        //  var header = document.createElement('h2');
+        //  var headerContent = document.createTextNode("Overview");
+        //  header.appendChild(headerContent)
+        //  tabWrapper.appendChild(header)
+        //}
     
-        if(document.getElementById("overview-tab")  !== null){
-          document.getElementById("overview-tab").remove()
-        }else{
-          var header = document.createElement('h2');
-          var headerContent = document.createTextNode("Overview");
-          header.appendChild(headerContent)
-          tabWrapper.appendChild(header)
-        }
+        //tableElement = overview2htmltab(tableContent)
     
-    
-    
-        tableElement = overview2htmltab(tableContent)
-       
-    
-        tabWrapper.appendChild(tableElement)
+        //tabWrapper.appendChild(tableElement)
     
     
-      // Create image if it doesn't already exist i
+      // Create image if it doesn't already exist 
       } else {
     
         if(document.getElementById("graph-image-" + title)  === null){
@@ -88,35 +98,6 @@ createApp({
   // Methods
   methods : {
 
-    overview2htmltab(overview){
-
-      var tableElement = document.createElement('table');
-      tableElement.id = "overview-tab";
-
-      for(let k in overview){
-
-        var rowElement = document.createElement("tr");
-        rowElement.classList.add("overview-tab-r")
-
-        var keyElement = document.createElement("td");
-        keyElement.classList.add("tab-item-key")
-        keyElement.appendChild(document.createTextNode(k))
-
-        var valElement = document.createElement("td");
-        valElement.classList.add("tab-item-val")
-        valElement.appendChild(document.createTextNode(overview[k]))
-
-        rowElement.appendChild(keyElement)
-        rowElement.appendChild(valElement)
-
-        tableElement.appendChild(rowElement)
-
-      }
-
-      return tableElement;
-
-
-    }
   }
 }).mount('#app');
 
