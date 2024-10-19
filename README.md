07c8e95ecba0ea6ab16dfd3828adcbcc2d175d9a
Eric McKanna <ewmckanna@gmail.com>
2024-10-19 14:10:25 -0400

    Add rye project management

--- README.md
+++ README.md
@@ -5,25 +5,34 @@ _A tool to display live visualizations of your pymoo projects in real time_
 ## Development Setup
 
 ### Python requirements
-You will need to install Python (and preferably Anaconda). Once your environment is set up, you will need to run:
+You will need to install [Rye](https://rye.astral.sh/guide/installation/):
 
 ```shell
-pip install pymoo Flask 
+curl -sSf https://rye.astral.sh/get | bash
+```
+
+Then enter the pymoo-dashboard directory and sync rye:
+
+```shell
+rye sync
 ```
 
 ### Node requirements
-This project uses Node, Nuxt, and Vue to build its front end UI. So to start: 
+This project uses Node, Nuxt, and Vue to build its front end UI. So to start:
 1. Install Node.js
 2. Enter the front end directory `cd frontend`
 3. Run `npm install`
 
 ## Running for development
 
-The following command will run a sample optimization problem.
+To run a sample optimization problem, activate the virtual environment and run `dashboard`.
 ```
-python Dashboard.py
+source .venv/bin/activate
+dashboard
 ```
 
+To save the hassle of activating the virtual environment manually, consider installing a shell plugin
+like [zsh-autoswitch-virtualenv](https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv).
 
 ## Building the project
 For Unix based OSes
@@ -31,15 +40,14 @@ For Unix based OSes
 ./build.sh
 ```
 
-
 ## Deploying to pymoo
 
-Simply set the Pymoo callback function as `Dashboard()` and you're good to go. 
-Make sure the `Dashboard.html`, `Dashboard.js`, and `Dashboard.css` are all in the 
+Simply set the Pymoo callback function as `Dashboard()` and you're good to go.
+Make sure the `Dashboard.html`, `Dashboard.js`, and `Dashboard.css` are all in the
 same directory as whereever `Dashboard.py` is deployed to.
 
-## Example: 
-```python         
+## Example:
+```python
     # create the reference directions to be used for the optimization
     ref_dirs = get_reference_directions("das-dennis", 3, n_partitions=12)
 
@@ -54,4 +62,3 @@ same directory as whereever `Dashboard.py` is deployed to.
                    callback=Dashboard(),
                    termination=('n_gen', 600))
 ```
-
--- src/pymoo_dashboard/Dashboard.py
+++ src/pymoo_dashboard/Dashboard.py
@@ -1,28 +1,23 @@
-from flask import Flask, request
-from flask_socketio import SocketIO
-import time
-import io
-import os
-import json
 import base64
-import threading
+import io
 import json
+import os
+import threading
+import time
 import webbrowser
 
-from pymoo.visualization.scatter import Scatter
-from pymoo.visualization.pcp import PCP
-
 import matplotlib.pyplot as plt
-import numpy as np
-
-from pymoo.algorithms.soo.nonconvex.ga import GA
-from pymoo.algorithms.moo.nsga2 import NSGA2
+from flask import Flask, request
+from flask_socketio import SocketIO
 from pymoo.algorithms.moo.nsga3 import NSGA3
-from pymoo.problems import get_problem
 from pymoo.core.callback import Callback
-from pymoo.optimize import minimize
 from pymoo.indicators.hv import Hypervolume
+from pymoo.optimize import minimize
+from pymoo.problems import get_problem
 from pymoo.util.ref_dirs import get_reference_directions
+from pymoo.visualization.pcp import PCP
+from pymoo.visualization.scatter import Scatter
+
 
 class Dashboard(Callback):
     def __init__(self, open_browser=False, develop=False, **kwargs) -> None:
@@ -31,113 +26,115 @@ class Dashboard(Callback):
         self.paused = False
         self.announcer = self.MessageAnnouncer()
         self.flask_thread = threading.Thread(target=self.start_server, daemon=True)
-        self.flask_thread.start() 
-    
+        self.flask_thread.start()
+
         self.develop = develop
 
         url = "localhost"
-        if develop: 
+        if develop:
             url += ":3000"
-            self.develop_thread = threading.Thread(target=self.start_dev_server)  
+            self.develop_thread = threading.Thread(target=self.start_dev_server)
             self.develop_thread.start()
-        else: 
+        else:
             url += ":5000"
-    
+
         if open_browser:
             threading.Timer(1.25, lambda: webbrowser.open(url)).start()
 
         # Default visualizations + user defined ones
-        self.visualizations = dict({
-            "HV": self.plot_hv,
-            "Pareto Front Scatter Plot": self.plot_scatter,
-            "PCP Plot": self.plot_pcp
-
-                }, **kwargs)
+        self.visualizations = dict(
+            {
+                "HV": self.plot_hv,
+                "Pareto Front Scatter Plot": self.plot_scatter,
+                "PCP Plot": self.plot_pcp,
+            },
+            **kwargs,
+        )
 
         # User defined visualizations
-        self.overview_fields = ['algorithm', 'problem', 'generation', 'seed', 'pop_size']
-    
-    def start_dev_server(self): 
-        
+        self.overview_fields = [
+            "algorithm",
+            "problem",
+            "generation",
+            "seed",
+            "pop_size",
+        ]
+
+    def start_dev_server(self):
         # Go to the path "nuxt-module"
         os.chdir("frontend")
 
         # Run the development server
         os.system("npm run dev")
 
-
     def notify(self, algorithm):
         while self.paused:
             time.sleep(1)
-        # PO values 
+        # PO values
         pf = algorithm.pop.get("F")
 
-        # HV reference point 
+        # HV reference point
         self.hv_ref_point = algorithm.problem.nadir_point()
-        if  self.hv_ref_point is None: 
+        if self.hv_ref_point is None:
             self.hv_ref_point = [1 for a in range(algorithm.problem.n_obj)]
 
-        # HV values 
+        # HV values
         hv_indicator = Hypervolume(ref_point=self.hv_ref_point)
         self.data["HV"].append(hv_indicator.do(pf))
-
 
         ## Overview table
         # Build the data for the overview table
         overview_dict = {}
 
         for o in self.overview_fields:
-
-            overview_func =  getattr(self, "overview_" + o)
+            overview_func = getattr(self, "overview_" + o)
 
             overview_dict[o] = overview_func(algorithm)
 
         # Send off overview table
         self.announcer.announce(plot_title="Overview", content=overview_dict)
-        
+
         ## Dashboard tables
         for v in self.visualizations:
-
             plotter = self.visualizations[v]
 
             fig = plotter(self, algorithm)
 
-            # Set up I/O buffer to save the image 
+            # Set up I/O buffer to save the image
             buffer = io.BytesIO()
 
-            fig.savefig(buffer, format='png', dpi=100)
+            fig.savefig(buffer, format="png", dpi=100)
 
             plt.close(fig)
 
             buffer.seek(0)
 
             # Encode bytes
-            plot_base64 = base64.b64encode(buffer.read()).decode('utf-8')
+            plot_base64 = base64.b64encode(buffer.read()).decode("utf-8")
 
             self.announcer.announce(plot_title=v, content=plot_base64)
-
 
     def start_server(self):
         self.app = Flask(__name__)
         self.socketio = SocketIO(self.app, cors_allowed_origins="*")
 
-        @self.app.route('/pause', methods=['POST'])
+        @self.app.route("/pause", methods=["POST"])
         def pause():
             self.paused = not self.paused
             self.announcer.pause(self.paused)
             if self.paused:
-                return 'Paused'
+                return "Paused"
             else:
-                return 'Unpaused'
+                return "Unpaused"
 
         @self.socketio.event
         def connect():
-            print(f'Client {request.sid} connected')
+            print(f"Client {request.sid} connected")
             self.announcer.set_socketio(self.socketio, request.sid, self.paused)
 
         @self.socketio.event
         def disconnect():
-            print('Client disconnected')
+            print("Client disconnected")
 
         self.socketio.run(self.app, port=5000)
 
@@ -145,26 +142,30 @@ class Dashboard(Callback):
         def __init__(self):
             self.socketio = None
             self.historical = []
-        
+
         def set_socketio(self, socketio, sid, paused):
             if not self.socketio:
                 self.socketio = socketio
-            self.socketio.emit('initial_data', {'msg': json.dumps(self.historical)}, room=sid)
-            self.socketio.emit('pause', {'msg': json.dumps(paused)})
+            self.socketio.emit(
+                "initial_data", {"msg": json.dumps(self.historical)}, room=sid
+            )
+            self.socketio.emit("pause", {"msg": json.dumps(paused)})
 
-        def announce(self, plot_title, content): 
+        def announce(self, plot_title, content):
             self.historical.append({"title": plot_title, "content": content})
-            if (self.socketio):
-                self.socketio.emit('update', {'msg': json.dumps({"title": plot_title, "content": content})})
-        
+            if self.socketio:
+                self.socketio.emit(
+                    "update",
+                    {"msg": json.dumps({"title": plot_title, "content": content})},
+                )
+
         def pause(self, paused):
-            if (self.socketio):
-                self.socketio.emit('pause', {'msg': json.dumps(paused)})
+            if self.socketio:
+                self.socketio.emit("pause", {"msg": json.dumps(paused)})
 
     ## Dashboard plots
     @staticmethod
     def plot_scatter(context, algorithm):
-
         # Send PO update to client
         F = algorithm.pop.get("F")
         plot = Scatter().add(F)
@@ -172,31 +173,27 @@ class Dashboard(Callback):
 
         return plot.fig
 
-
     @staticmethod
     def plot_pcp(context, algorithm):
-
         # Send PO update to client
         F = algorithm.pop.get("F")
         plot = PCP().add(F)
         plot.plot_if_not_done_yet()
-   
-        return plot.fig
 
+        return plot.fig
 
     @staticmethod
     def plot_hv(context, algorithm):
-
         # Send PO update to client
         hv_series = context.data["HV"]
         gen_series = list(range(len(hv_series)))
 
-        plt.figure(figsize=(7,7))
+        plt.figure(figsize=(7, 7))
         plt.plot(gen_series, hv_series)
-        plot = plt.gcf() 
+        plot = plt.gcf()
 
         return plot
-        
+
     ## Overview functions
     @staticmethod
     def overview_problem(algorithm):
@@ -218,32 +215,35 @@ class Dashboard(Callback):
     def overview_pop_size(algorithm):
         return len(algorithm.pop)
 
-if __name__ == "__main__": 
 
+def main():
+    # 2 dimension example
+    # problem = get_problem("zdt2")
 
-    # 2 dimension example 
-    #problem = get_problem("zdt2")
+    # algorithm = NSGA2(pop_size=100)
 
-    #algorithm = NSGA2(pop_size=100)
-
-    #res = minimize(problem,
+    # res = minimize(problem,
     #               algorithm,
     #               ('n_gen', 200),
     #               seed=1,
     #               callback=Dashboard(),
     #               verbose=True)
 
-        
     # create the reference directions to be used for the optimization
     ref_dirs = get_reference_directions("das-dennis", 3, n_partitions=12)
 
     # create the algorithm object
-    algorithm = NSGA3(pop_size=92,
-                      ref_dirs=ref_dirs)
+    algorithm = NSGA3(pop_size=92, ref_dirs=ref_dirs)
 
     # execute the optimization
-    res = minimize(get_problem("dtlz1"),
-                   algorithm,
-                   seed=2018194,
-                   callback=Dashboard(develop=True),
-                   termination=('n_gen', 600))
+    _ = minimize(
+        get_problem("dtlz1"),
+        algorithm,
+        seed=2018194,
+        callback=Dashboard(develop=True),
+        termination=("n_gen", 600),
+    )
+
+
+if __name__ == "__main__":
+    main()
